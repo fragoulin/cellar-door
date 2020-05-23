a0ec7707ae57bacde0b62eefb2ebea4bba1258cf
fragoulin <fabrice.morin@live.com>
2020-05-23 11:09:29 +0200

    Remove property according to electron update to v9

--- src/main.ts
+++ src/main.ts
@@ -130,7 +130,6 @@ app.on('activate', () => {
 
 // In this file you can include the rest of your app's specific main process
 // code. You can also put them in separate files and require them here.
-app.allowRendererProcessReuse = true
 
 // Register IPC listeners for main process.
 ipcMainService.registerListeners()
