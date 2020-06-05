e7270ee941c2ee2ad8877b204110292f0dac1388
fragoulin <fabrice.morin@live.com>
2020-06-05 22:18:29 +0200

    Confirmation dialog before removing emulator

--- app/electron/menu/context-menu.ts
+++ app/electron/menu/context-menu.ts
@@ -1,5 +1,10 @@
 import { Options } from 'electron-context-menu'
-import { BrowserWindow, MenuItemConstructorOptions } from 'electron'
+import {
+  BrowserWindow,
+  dialog,
+  MenuItemConstructorOptions,
+  MessageBoxOptions,
+} from 'electron'
 import { i18n as I18n } from 'i18next'
 import { MenuClickChannel, RemoveEmulatorId } from '../constants'
 
@@ -14,14 +19,39 @@ const contextMenuOptions = (i18n: I18n): Options => {
       browserWindow
     ): MenuItemConstructorOptions[] => [
       {
-        label: i18n.t('emulatorsList.removeEmulator'),
+        label: i18n.t('emulatorsList.removeEmulator.label'),
         visible: params.linkURL.match(/\/emulator\/[a-z]+$/) !== null,
         click: (): void => {
           const match = params.linkURL.match(/\/emulator\/([a-z]+)$/)
           if (match) {
             const emulatorId = match[1]
-            const win = browserWindow as BrowserWindow
-            win.webContents.send(MenuClickChannel, RemoveEmulatorId, emulatorId)
+
+            // Confirmation dialog
+            const options: MessageBoxOptions = {
+              buttons: [i18n.t('common.yes'), i18n.t('common.no')],
+              title: i18n.t('emulatorsList.removeEmulator.label'),
+              message: i18n.t('emulatorsList.removeEmulator.confirmation', {
+                name: emulatorId,
+              }),
+              detail: i18n.t('emulatorsList.removeEmulator.confirmationDetail'),
+              type: 'question',
+              defaultId: 1,
+              cancelId: 1,
+              noLink: true,
+            }
+            dialog
+              .showMessageBox(options)
+              .then((value) => {
+                if (value.response === 0) {
+                  const win = browserWindow as BrowserWindow
+                  win.webContents.send(
+                    MenuClickChannel,
+                    RemoveEmulatorId,
+                    emulatorId
+                  )
+                }
+              })
+              .catch(console.error)
           }
         },
       },
