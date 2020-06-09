12f131310e746d4fc0c1ffaf3a50d6e6da263321
fragoulin <fabrice.morin@live.com>
2020-06-09 19:08:43 +0200

    Use material table to display emulator configuration

--- app/src/components/emulator/configuration/emulator-configuration-summary/emulator-configuration-summary-component.tsx
+++ app/src/components/emulator/configuration/emulator-configuration-summary/emulator-configuration-summary-component.tsx
@@ -2,6 +2,14 @@ import './emulator-configuration-summary.scss'
 import React from 'react'
 import { EmulatorConfiguration } from 'models/emulator/types'
 import { withTranslation, WithTranslation } from 'react-i18next'
+import {
+  TableContainer,
+  Table,
+  TableBody,
+  TableRow,
+  TableCell,
+} from '@material-ui/core'
+import Paper from '@material-ui/core/Paper'
 
 /**
  * Properties definition for this component.
@@ -23,20 +31,22 @@ class EmulatorConfigurationSummary extends React.Component<
    */
   public render(): React.ReactNode {
     return (
-      <table>
-        <tbody>
-          {this.props.configuration.map(
-            (configuration: EmulatorConfiguration) => {
-              return (
-                <tr key={configuration.name}>
-                  <td>{configuration.name}</td>
-                  <td>{configuration.value}</td>
-                </tr>
-              )
-            }
-          )}
-        </tbody>
-      </table>
+      <TableContainer component={Paper}>
+        <Table aria-label="emulator configuration">
+          <TableBody>
+            {this.props.configuration.map(
+              (configuration: EmulatorConfiguration) => {
+                return (
+                  <TableRow key={configuration.name}>
+                    <TableCell>{configuration.name}</TableCell>
+                    <TableCell>{configuration.value}</TableCell>
+                  </TableRow>
+                )
+              }
+            )}
+          </TableBody>
+        </Table>
+      </TableContainer>
     )
   }
 }
