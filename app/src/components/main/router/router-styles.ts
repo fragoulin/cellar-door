42a52e7bfcde2d941aa31e93f5cbdeaecc50d728
fragoulin <fabrice.morin@live.com>
2020-06-11 17:42:02 +0200

    Fix height

--- app/src/components/main/router/router-styles.ts
+++ app/src/components/main/router/router-styles.ts
@@ -3,7 +3,7 @@ import { makeStyles, createStyles } from '@material-ui/core'
 const useStyles = makeStyles(() =>
   createStyles({
     root: {
-      height: '94vh',
+      height: '100vh',
     },
   })
 )
